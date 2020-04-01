d82f50c9281386576153d2031b2333ad72d40d6a
Kshounish <46402961+Kshounish@users.noreply.github.com>
2020-04-01 16:17:17 +0530

    Add files via upload

---
+++ CircleArea.c
@@ -0,0 +1,14 @@
+/* Swap without 3rd variable*/
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   float r, area;
+   printf("\nEnter the radius::");
+   scanf("%f",&r);
+   area= 3.14159*r*r;
+   printf("\n The area of circle is::%f",area);
+  getch();
+}
+
