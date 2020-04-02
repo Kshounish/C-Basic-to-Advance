4d5196a2b8c69a3c6978f67f263edd5b897e660c
Kshounish <46402961+Kshounish@users.noreply.github.com>
2020-04-02 11:04:24 +0530

    Add files via upload

---
+++ Digit Counter.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main () 
+  {
+   int a, count = 0;
+   printf ("Enter the number::");
+   scanf ("%d", &a);
+    while (a != 0)	//when the number "a"becomes zero it jumps out of the loop and count value is printed
+     {
+         if (a = a / 10);	// Since 10 is divided by each number.Once a number is divided it is removed  
+         count++;			//once a num is divided the count is increased by 1 and continues untill the number "a" is not reduced to 0
+     }
+   printf ("The total digits are::%d", count);
+   return 0;
+  }
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
